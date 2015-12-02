888d597ee7db07ab38a56bf2b08ee27cadbcb46b
Alejandro Tocar <aletocar@hotmail.com>
2015-12-02 20:01:53 -0300

    Documentation added

--- build/exploded-app/WEB-INF/groovy/services/templateService.groovy
+++ build/exploded-app/WEB-INF/groovy/services/templateService.groovy
@@ -24,16 +24,18 @@ class TemplateService
 	<!--IMG ARTICULO -->
 	<div style="width: 100%;">
 		<img id="article-image" src="" style="margin: 30px auto 20px auto; max-width: 918px; height: auto;" alt="aqui irá la imagen de tu artículo">
+		<p>This is the placeholder for your article image</p>
 	</div>
 
 	<!--IMG TITULO CARACTERISTICAS -->
 	<div style="width: 100%; ">
 		<img id="title-image" src="" style="margin: 0; width: 100%;" alt="Selecciona la imagen de fondo del título">
+		<p>This is the placeholder for background image</p>
 	</div>
 
 	<!-- TITULO ARTICULO -->
 	<div style="width: 100%;">
-		<div style="margin-left: 20%;margin-right: 20%;width: 60%;background: -webkit-linear-gradient(#B5B5B9, #636161, #B5B5B9);">
+		<div style="margin-left: 20%;margin-right: 20%;width: 60%;background: #E7E7E7;">
 			<p id="title-article-text" style = "color:black; width: 100%;font-size: 30px;font-weight: bold;text-align: center;padding-top: 5%;padding-bottom: 5%;margin: 0;">
 			Tu título aquí
 			</p>
